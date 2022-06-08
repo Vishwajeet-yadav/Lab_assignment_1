76024149e4c88b1bec55ba62ab79b92bf0469dbb
Vishwajeet <103632397+Vishwajeet-yadav@users.noreply.github.com>
2022-06-08 14:03:39 +0530

    Update assignment_3.py

--- assignment_3.py
+++ assignment_3.py
@@ -41,23 +41,23 @@ print((y2-y1)/(x2-x1))
 
 # (a)
 for i in range(5):
-    print(i,end=",")
+    print(i,end=" ")
 print()
 # (b)
 for i in range(3,10):
-    print(i,end=",")
+    print(i,end=" ")
 print()
 # (c)
 for i in range(4,13,3):
-    print(i,end=",")
+    print(i,end=" ")
 print()
 # (d)
 for i in range(15,5,-2):
-    print(i,end=",")
+    print(i,end=" ")
 print()
 # (e)
 for i in range(5,3):
-    print(i,end=",")
+    print(i,end=" ")
 print()
 
 
